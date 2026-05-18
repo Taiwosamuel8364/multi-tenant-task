7c3ca2ebb2496102fccb7289dccf501404156d72
Taiwosamuel8364 <taiwosamuel8364@gmail.com>
2026-05-18 23:16:08 +0100

    feat(swagger): add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configurtion
+  const config = new DocumentBuilder()
+    .setTitle("Multi-Tenant Task")
+    .setDescription("Api Documnetaion fot Multi Tenant Task system")
+    .setVersion('1.0')
+    .addTag('Task')
+    .build();
+  
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
